9bc563a5c4f6909a1aa51c4154377573d2c3b12d
TimGilm <tgilmutdinov@mail.ru>
2023-11-01 23:07:42 +0500

    Go file was refined according to example shown in 20.1 module (hometask explanation)

--- skillbox19_hometask1_arraymerge.go
+++ skillbox19_hometask1_arraymerge.go
@@ -1,43 +1,46 @@
 /*
 	Задание 1. Слияние отсортированных массивов
 
-Что нужно сделать Напишите функцию, которая производит слияние двух отсортированных массивов длиной четыре и пять в один массив длиной девять.
+Что нужно сделать Напишите функцию, которая производит слияние двух отсортированных массивов
+длиной четыре и пять в один массив длиной девять.
 package main
 */
 package main
 
 import "fmt"
 
-var arrayOne = [4]int{1, 3, 6, 9}
-var arrayTwo = [5]int{2, 4, 5, 7, 8}
-var arrayUnited [9]int
+const firstArrSize = 4
+const secondArrSize = 5
+const thirdArrSize = 9
 
-func arrayMerge([4]int, [5]int) [9]int {
-	n := 0
-	for i := 0; i < len(arrayOne); i++ {
-		if arrayOne[i] < arrayTwo[i] {
-			arrayUnited[n] = arrayOne[i]
-			n += 1
-			arrayUnited[n] = arrayTwo[i]
-			n += 1
+var arrayOne = [firstArrSize]int{1, 3, 6, 9}
+var arrayTwo = [secondArrSize]int{2, 4, 5, 7, 8}
+
+func arrayMerge(A [firstArrSize]int, B [secondArrSize]int) [thirdArrSize]int {
+	var result [thirdArrSize]int
+	a := 0
+	b := 0
+	for i := 0; i < thirdArrSize; i++ {
+		if a >= len(A) {
+			result[i] = B[b]
+			b++
+			continue
+		} else if b >= len(B) {
+			result[i] = A[a]
+			a++
+			continue
+		}
+		if A[a] > B[b] {
+			result[i] = B[b]
+			b++
 		} else {
-			arrayUnited[n] = arrayTwo[i]
-			n += 1
-			arrayUnited[n] = arrayOne[i]
-			n += 1
+			result[i] = A[a]
+			a++
 		}
 	}
-	m := len(arrayTwo) - 1
-	arrayUnited[n] = arrayTwo[m]
-	if arrayUnited[n] < arrayUnited[n-1] {
-		arrayUnited[n], arrayUnited[n-1] = arrayUnited[n-1], arrayUnited[n]
-	}
-	return arrayUnited
+	return result
 }
 
 func main() {
-
-	arrayMerge(arrayOne, arrayTwo)
-	fmt.Print(arrayUnited)
-
+	fmt.Print(arrayMerge(arrayOne, arrayTwo))
 }
